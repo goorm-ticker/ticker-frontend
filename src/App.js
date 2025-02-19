1b7010c23ec7bd92850b9227946270c6808b76c6
Hm-source <swo8852@gmail.com>
2025-02-20 02:29:01 +0900

    feat: 리우터 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import Map from './components/Map'; // Map 컴포넌트 불러오기
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Map from './components/Map';
+import ReservationForm from './components/ReservationForm';
+import ReservationSuccess from './components/ReservationSuccess';
+import Register from './components/Register';
+import Login from './components/Login';
+import MyReservations from './components/Myreservations';
+import Header from './components/Header'; 
 import './App.css';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const user = sessionStorage.getItem("user");
+    setIsAuthenticated(!!user);
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Ticker</h1>
-      <Map />
-    </div>
+    <Router>
+      <Header isAuthenticated={isAuthenticated} />
+      <Routes>
+        <Route path="/" element={<Map />} />
+        <Route path="/reservation/:id" element={<ReservationForm />} />
+        <Route path="/reservation-success" element={<ReservationSuccess />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/myreservations" element={<MyReservations />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
